a8651ae200bc73f8c131642177b8cc433925ed83
propanaaz <propanaaz@gmail.com>
2026-05-11 12:47:21 +0100

    update

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>{{ __('common.Version') }}</b> 3.0.3
+      <b>Designed By</b> Propa Technologies
     </div>
-    <strong>{{ __('common.Copyright') }} &copy; {{ date('Y') }} <a href="http://github.com/angkosal">Kosal</a>.</strong> {{ __('common.all_reserved') }}
+    <strong>{{ __('common.Copyright') }} &copy; {{ date('Y') }} </strong> {{ __('common.all_reserved') }}
   </footer>
